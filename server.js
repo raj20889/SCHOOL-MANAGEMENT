baee34e35ea63e28ae40eb7ee2981fae5e8d8dd6
Mohit Raj <rajm20889@gmail.com>
2025-03-29 13:39:36 +0530

    test db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2'); // Added mysql2 import
+ // Added mysql2 import
 require('dotenv').config(); // Load environment variables
 
 const schoolRoutes = require('./routes/schoolRoutes'); // Import school routes
@@ -13,24 +13,9 @@ app.use(cors());
 app.use(express.json()); // Enable JSON body parsing
 
 // Create a connection to the database
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
 
-// Test database connection
-app.get('/test-db', (req, res) => {
-  db.connect((err) => {
-      if (err) {
-          res.status(500).send('Database connection failed: ' + err.message);
-      } else {
-          res.send('Database connected successfully!');
-      }
-  });
-});
+
+
 
 // Root route
 app.get('/', (req, res) => {
